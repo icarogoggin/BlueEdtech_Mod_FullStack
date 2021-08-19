3281aad00813fe74672dbb63bc950df254ee8884
icarogoggin <icaro.goggin@gmail.com>
2021-08-19 19:32:13 -0300

    Aula 02

---
+++ aula02/projeto02/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+const games = [
+    'Skyrim',
+    'Ragnarok',
+    'Civilization'
+
+];
+
+const msgInicio = [
+    'Bem vindo',
+    'Ola amigo, bem vindo ao servidor',
+    'Servidor de jogos',
+    'Este é meu servidor',
+];
+
+function randomMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function frase(num){
+    return msgInicio[num];
+}
+
+const msg = "teste"
+
+app.get('/',(req, res) => {
+    res.send(`<h1>${frase(randomMinMax(0,msgInicio.length))}</h1>`);
+});
+
+games.forEach(function (item, indice){
+    console.log(item, indice);
+});
+
+app.get('/games/:id', (req, res) => {
+    const id = req.params.id;
+    const game = games[id-1];
+    if (id > games.length || id < 1){
+        res.send("ID invalido, tente novamente");
+    }else {
+        res.send(game);
+    }
+});
+
+app.listen(port, () => {
+    console.info(`App esta rodando em: http://localhost:${port}/`);
+});
